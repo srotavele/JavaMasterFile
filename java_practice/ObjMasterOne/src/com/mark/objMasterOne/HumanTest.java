9bdfac0eb8ed9925df4394608740e1dcdb7e5ca5
MRea <83099810+srotavele@users.noreply.github.com>
2021-08-04 01:46:42 -0700

    Add files via upload

---
+++ java_practice/ObjMasterOne/src/com/mark/objMasterOne/HumanTest.java
@@ -0,0 +1,13 @@
+package com.mark.objMasterOne;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human dude1 = new Human();
+		Human dude2 = new Human();
+		
+		dude1.attack(dude2);
+
+	}
+
+}
